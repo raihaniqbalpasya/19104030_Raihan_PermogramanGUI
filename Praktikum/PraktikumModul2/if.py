ff0524261c384d962f3da025ea074092a7fc9a04
Raihan Iqbal Pasya <72422130+raihaniqbalpasya@users.noreply.github.com>
2021-04-18 22:34:21 +0700

    Add files via upload

---
+++ Praktikum/PraktikumModul2/if.py
@@ -0,0 +1,20 @@
+x = False
+y = True
+
+# if..else
+if x != True:
+    print('print')
+else:
+    print('tidak print')
+
+# if..else if..else
+if x != False :
+    print('print X')
+elif y != False:
+    print('print Y')
+
+# if(..and..)..else
+if x != True and y == False :
+    print('print')
+else:
+    print('tidak print')
