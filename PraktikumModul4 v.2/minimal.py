89c2e1257b3f3bd4a60e1f1be17d54360728306e
Raihan Iqbal Pasya <72422130+raihaniqbalpasya@users.noreply.github.com>
2021-05-24 22:44:04 +0700

    Add files via upload

---
+++ PraktikumModul4 v.2/minimal.py
@@ -0,0 +1,11 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
+from MinimalForm import *
+
+if __name__ == '__main__':
+    a = QApplication(sys.argv)
+    minform = MinimalForm()
+    minform.show()
+    a.exec_()
